d2500c2845433446802289d37a2bf74d5f88ca65
Jhonatan Santana <codejhonatansantana@gmail.com>
2024-07-30 17:05:42 -0300

    doc: turned to installable binary #9

---
+++ dundie/__main__.py
@@ -0,0 +1,5 @@
+print("Executing entry point for dundie...")
+
+
+def main():
+    pass
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages 
+
+setup( 
+    name="dundie",
+    version="0.1.0",
+    description="Reward point system for Dunder Mifflin",
+    author="Jhônatan Santana",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "dundie = dundie.__main__:main"
+        ]
+    }
+ )
